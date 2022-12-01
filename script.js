75545925500d24b54655ed771916dd3098e1bac9
Dev Johari <102499062+devjohari@users.noreply.github.com>
2022-12-01 11:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let textfield = document.getElementById("textfield");
 let buttons = document.querySelectorAll(".butt");
+document.addEventListener("keydown", (e) => {
+   if (e.key == "Enter") {
+      buttons[buttons.length - 1].click();
+   } else if (e.key == "Backspace") {
+      let arr = textfield.value.split("");
+      arr.pop();
+      textfield.value = arr.join("");
+   } else if (e.key == "Delete") {
+      buttons[3].click();
+   } else {
+      buttons.forEach((i) => {
+         if (i.innerText == e.key) {
+            i.click();
+         }
+      });
+   }
+});
+
 buttons.forEach((i) => {
    i.onclick = () => {
       if (i.innerText != "C" && i.innerText != "=") {
@@ -11,8 +29,8 @@ buttons.forEach((i) => {
       }
    };
 });
-document.addEventListener("keypress", (e) => {
-   if (e.key == "Enter") {
-      buttons[buttons.length - 1].click();
-   }
-});
+
+let hello = "mynameisdev";
+let helloa = hello.split("");
+helloa.pop();
+console.log(helloa.join(""));
